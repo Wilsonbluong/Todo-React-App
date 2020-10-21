b3b6ae06f27b08cd22a2a324c652bf857ece64aa
Wilson Luong <wilsonbluong@gmail.com>
2020-10-21 11:31:49 -0700

    Updates

---
+++ precache-manifest.90d1c32979cbb8932ea52fbd37298084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02df74a408e1a3a410acfc2c4f3f922",
+    "url": "/Todo-React-App/index.html"
+  },
+  {
+    "revision": "18d91e78079509a421d5",
+    "url": "/Todo-React-App/static/css/main.4d8ab585.chunk.css"
+  },
+  {
+    "revision": "f424a031940ddd1fe08b",
+    "url": "/Todo-React-App/static/js/2.216afb14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-React-App/static/js/2.216afb14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18d91e78079509a421d5",
+    "url": "/Todo-React-App/static/js/main.161ed90c.chunk.js"
+  },
+  {
+    "revision": "83a5cbd72bbfe54a5a0b",
+    "url": "/Todo-React-App/static/js/runtime-main.7dd880e4.js"
+  }
+]);
